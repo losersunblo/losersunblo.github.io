2467565303fdfa9d3e31465946b2b370a820a6b7
losersunblo <125787075+losersunblo@users.noreply.github.com>
2023-07-10 12:42:07 -0400

    Add files via upload

---
+++ assets/js/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'.1Losers1','/prediction','/varity','immortal','best dev','crypto mastermind','paster',],
+  typeSpeed : 40,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
